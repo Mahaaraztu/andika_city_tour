6254feb0ec7bfd40e64916eea9a845a2d3e94942
Mahaaraztu <andika.maharaztu_1@mhs.mdp.ac.id>
2023-10-30 10:39:01 +0700

    uts_andika
    uts

---
+++ android/app/src/main/kotlin/com/example/andika/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.andika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen.dart
@@ -0,0 +1,12 @@
+import 'package:city_tour/city.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extend StatelessWidget{
+city city;
+
+DetailScreen({super.key, required this city});
+
+@override
+widget build(BuildContext context){
+return Scaffold()
+
